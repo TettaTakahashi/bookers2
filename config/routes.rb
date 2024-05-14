55d4625ba5998f7f8b5616a285ca171ea56849b5
TettaTakahashi <x4tetta@gmail.com>
2024-05-14 11:13:35 +0000

    [Update]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'books'=> 'books#index'
   get 'users'=> 'users#index'
   
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+  
   resources :books, only: [:new, :index, :show, :index, :create, :update, :edit, :destroy]
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
